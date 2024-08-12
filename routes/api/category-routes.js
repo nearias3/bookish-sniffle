d44086e95eda870335781406d89c4c53fbfa12f6
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-12 14:14:09 -0500

    Completed code for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,12 +44,44 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatedCategory) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedCategory) {
+      res.status(400).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deletedCategory);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
